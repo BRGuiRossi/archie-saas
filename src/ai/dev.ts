07346b9b3775c4fb9cd333f2f5a338127e41e896
Guilherme Rossi <guilherme@solarbh.com.br>
2025-08-30 20:07:39 +0000

    > Build failed because of webpack errors Restoring original next config

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,4 +2,3 @@ import { config } from 'dotenv';
 config();
 
 import '@/ai/flows/extract-tasks-from-document.ts';
-import '@/ai/flows/summarize-document.ts';
--- src/ai/flows/create-tasks-in-clickup.ts
+++
@@ -1,5 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/extract-tasks-from-document.ts';
-import '@/ai/flows/summarize-document.ts';
